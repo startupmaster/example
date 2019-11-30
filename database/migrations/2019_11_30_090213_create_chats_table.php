4805935b990775676def32d5ca60da834c62a233
Vladimir Korzhenevsky <vladimir.korzhenevsky@gmail.com>
2019-11-30 12:23:36 +0300

    add chat model/controller/migration

---
+++ database/migrations/2019_11_30_090213_create_chats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedBigInteger('user_created');
+            $table->unsignedBigInteger('user_updated');
+            $table->boolean('is_private');
+            $table->boolean('is_dropped');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
